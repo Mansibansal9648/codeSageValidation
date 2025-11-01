d8a44ac6e46c9228b8f6accc6534e32719fb8a1b
Mansi Bansal <54983818+Mansibansal9648@users.noreply.github.com>
2025-11-01 15:02:37 +0530

    basic ui setup (#6)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    basic UI setup to confirm project is running
+    </>
+  );
+}
+
+export default App;
